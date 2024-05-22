f4462ec5b875e759d0dea691801bd107290cda31
Edgar Parucho <e.parucho@outlook.com>
2024-05-22 16:35:06 -0400

    Create tracking form component accesible with authentication

---
+++ src/components/trackingForm/index.jsx
@@ -0,0 +1,9 @@
+function TrackingForm() {
+    return (
+        <form>
+            <input type='text' placeholder='New Habit' className='px-4 py-2 w-20' />
+        </form>
+    );
+}
+
+export default TrackingForm;
